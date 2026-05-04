7e2dd8f51ae49051085fffe2be93317892eaa026
eliasdagger <125106050+eliasdagger@users.noreply.github.com>
2026-05-03 21:42:03 -0400

    RA JSON export

--- ReportArchive.java
+++ ReportArchive.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ReportArchive{
     private ReportList allReports;
     private ReportTreeNode dateTreeRoot;
@@ -10,7 +13,7 @@ public class ReportArchive{
 
     public ReportTreeNode insertBST(ReportTreeNode node, Report r){
         if (node == null) {
-            return ReportTreeNode(r);
+            return new ReportTreeNode(r);
         }
         if (r.compareByDate(node.getData()) < 0){
             node.setLeft(insertBST(node.getLeft(), r));
@@ -21,31 +24,82 @@ public class ReportArchive{
     }
 
     public void reverseInOrderTraversal(ReportTreeNode node){
-        node.getRight();
-        allReports.addToFront(node.getData());
-        node.getLeft();
+        if (node == null) return;
+        reverseInOrderTraversal(node.getRight());
+        System.out.println(node.getData());
+        reverseInOrderTraversal(node.getLeft());
     }
 
     public void filterByCategory(String category){
-        browser = buildFromList(allReports);
-        
+        ReportNode node = allReports.getHead();
+        while (node != null) {
+            if (category.equals(node.getData().getCategory())) System.out.println(node.getData());
+            node = node.getNext();
+        }
     }
 
     public void filterByTag(String tag){
-
-    }
-
-    public void filterByDateRange(String start, String end){
-
+        ReportNode node = allReports.getHead();
+        while (node != null) {
+            for (String t : node.getData().getTags()){
+                if (t.equals(tag)){
+                    System.out.println(node.getDathis is ta());
+                    break
+                }
+            }
+            node = node.getNext();
+        }
     }
 
     public void filterByAuthor(String name){
-
+        ReportNode node = allReports.getHead();
+        while (node != null) {
+            if (name.equals(node.getData().getAuthor())) System.out.println(node.getData());
+            node = node.getNext();
+        }
     }
 
-    public void runMenu(){
-        
+
+
+    public void exportToJSON() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[\n");
+        buildJSON(dateTreeRoot, sb);
+        int lastComma = sb.lastIndexOf(",");
+        if (lastComma != -1) sb.deleteCharAt(lastComma);
+        sb.append("\n]");
+
+        try {
+            FileWriter fw = new FileWriter("reports.json");
+            fw.write(sb.toString());
+            fw.close();
+            System.out.println("Exported to reports.json successfully.");
+        } catch (IOException e) {
+            System.out.println("Export failed: " + e.getMessage());
+        }
     }
 
+    private void buildJSON(ReportTreeNode node, StringBuilder sb) {
+        if (node == null) return;
+        buildJSON(node.getRight(), sb);
+        Report r = node.getData();
+        sb.append("  {\n");
+        sb.append("    \"title\": \"" + r.getTitle() + "\",\n");
+        sb.append("    \"author\": \"" + r.getAuthor() + "\",\n");
+        sb.append("    \"category\": \"" + r.getCategory() + "\",\n");
+        sb.append("    \"date\": \"" + r.getDate() + "\",\n");
+        sb.append("    \"excerpt\": \"" + r.getExcerpt() + "\",\n");
+        sb.append("    \"readTime\": " + r.getReadTimeMinutes() + ",\n");
+        // tags array
+        sb.append("    \"tags\": [");
+        String[] tags = r.getTags();
+        for (int i = 0; i < tags.length; i++) {
+            sb.append("\"" + tags[i] + "\"");
+            if (i < tags.length - 1) sb.append(", ");
+        }
+        sb.append("]\n");
+        sb.append("  },\n");
+        buildJSON(node.getLeft(), sb);
+    }
 
 }
